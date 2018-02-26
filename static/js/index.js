09565667534a4602785f4b2f2572ebc03841a68f
nirmal <a.m.s.nirmal.a@gmail.com>
2018-02-26 17:03:36 +0530

    enrollment scripts added

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,61 @@
 $(document).ready(function(){
-	var result;
+
     $("button").click(function(){
         $('#bt').attr("disabled", true);
         var id = $('.getId').text();
+        var val = $("#btp").text();
+        var disition = "";
+        if(val === "Enroll"){
+            disition = "add";
+        }
+        else{
+            disition ="remove";
+        }
         $.ajax({
-            url: "/draws"+id+"/enrollments/add",
             type: "GET",
-            dataType:'application/json',
-            success: function(result, status, xhr) {
-                console.log(result);
-                console.log(status);
-                var val = $("#btp").text();
-                if(val === "enroll")
-                    document.getElementById("demo").innerHTML = "unenroll";
-                else
-                    document.getElementById("demo").innerHTML = "enroll";
-                $('#demo').attr("disabled", false);
-                $("#displaySu").html("Succes");
+            url: "/draws/"+id+"/enrollments/"+disition,
+            success: function(data) {
+                console.log(data);
+                $('#bt').attr("disabled", false);
+                if(disition === "add"){
+                    $("#btp").html("<button id =\"bt\" class = \"btn btn-outline-success\">Unenroll</button>");
+                }
+                else{
+                    $("#btp").html("<button id=\"bt\" class = \"btn btn-outline-success\">Enroll</button>");
+                }
+                var h1 = document.getElementById("hidden1");
+                h1.style.display = 'block' ;
             },
-            error: function(xhr, status, error) {
+            error: function(status) {
                 console.log(status);
-                
-                $("#displayEr").html("error");
-            } 
+                var h2 = document.getElementById("hidden2");
+                h2.style.display = 'block' ;
+            }, 
         });
     });
-});
+ });
+
+
+// $.ajax({
+//     type:"GET",
+//     url:"https://api.meetup.com/2/cities",
+//     success: function(data) {
+//       $('.text').text(JSON.stringify(data));
+//     },
+//     dataType: 'jsonp',
+//   });
+
+// $('#bt').attr("disabled", true);
+        // $.ajax({
+        //     type:"GET",
+        //     url:"https://api.meetup.com/2/cities",
+        //     success: function(data) {
+        //         $("#displaySu").html("Succes");
+        //         console.log(data);
+        //         $('#demo').attr("disabled", false);
+        //     },
+        //     dataType: 'jsonp',
+        //     error: function(){
+        //         console.log("error");
+        //         $('#demo').attr("disabled", false);
+        //     }
