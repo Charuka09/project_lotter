be515e60c6a5303d578a3e5d2c23c1af2dc2cf3d
nirmal <a.m.s.nirmal.a@gmail.com>
2018-02-27 13:36:01 +0530

    complet-js functions

--- static/js/index.js
+++ static/js/index.js
@@ -1,61 +1,46 @@
 $(document).ready(function(){
 
     $("button").click(function(){
-        $('#bt').attr("disabled", true);
-        var id = $('.getId').text();
-        var val = $("#btp").text();
+
+        var val = $(this).val();
+        var id = $( "button" ).index( this );
+
+        $('#bt'+id).attr("disabled", true);
+
         var disition = "";
-        if(val === "Enroll"){
+        if(val == "Enroll"){
             disition = "add";
         }
         else{
             disition ="remove";
         }
+
         $.ajax({
             type: "GET",
             url: "/draws/"+id+"/enrollments/"+disition,
             success: function(data) {
+
                 console.log(data);
-                $('#bt').attr("disabled", false);
+                $('#bt'+id).attr("disabled", false);
                 if(disition === "add"){
-                    $("#btp").html("<button id =\"bt\" class = \"btn btn-outline-success\">Unenroll</button>");
+                    $("#btp"+id)
+                        .html("<button id =\"bt\" class = \"btn btn-outline-success\" value = \"Unenroll\">Unenroll</button>");
                 }
                 else{
-                    $("#btp").html("<button id=\"bt\" class = \"btn btn-outline-success\">Enroll</button>");
+                    $("#btp"+id)
+                        .html("<button id=\"bt\" class = \"btn btn-outline-success\" value = \"Enroll\">Enroll</button>");
                 }
+
                 var h1 = document.getElementById("hidden1");
                 h1.style.display = 'block' ;
+
             },
             error: function(status) {
-                console.log(status);
+
                 var h2 = document.getElementById("hidden2");
                 h2.style.display = 'block' ;
+
             }, 
         });
     });
  });
-
-
-// $.ajax({
-//     type:"GET",
-//     url:"https://api.meetup.com/2/cities",
-//     success: function(data) {
-//       $('.text').text(JSON.stringify(data));
-//     },
-//     dataType: 'jsonp',
-//   });
-
-// $('#bt').attr("disabled", true);
-        // $.ajax({
-        //     type:"GET",
-        //     url:"https://api.meetup.com/2/cities",
-        //     success: function(data) {
-        //         $("#displaySu").html("Succes");
-        //         console.log(data);
-        //         $('#demo').attr("disabled", false);
-        //     },
-        //     dataType: 'jsonp',
-        //     error: function(){
-        //         console.log("error");
-        //         $('#demo').attr("disabled", false);
-        //     }
